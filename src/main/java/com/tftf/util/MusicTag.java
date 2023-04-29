ccc50aab9b6dec1fe034e7422bab698f4c5fe4a7
jm0s <woals1488@naver.com>
2023-04-29 17:27:05 +0900

    tag를 한 가지 상태로 저장하고 리스트 타입으로 반환하는 함수 생성

--- src/main/java/com/tftf/util/MusicTag.java
+++ src/main/java/com/tftf/util/MusicTag.java
@@ -5,18 +5,16 @@ import java.util.HashMap;
 
 public class MusicTag {
     public HashMap<CharSequence, CharSequence> tagMap;
-    public ArrayList<CharSequence> tagList;
 
     public MusicTag() {
         tagMap = new HashMap<>();
-        tagList = new ArrayList<>();
     }
 
     public int getSize() {
-        return tagList.size();
+        return tagMap.size();
     }
 
-    public CharSequence at(int index) {
-        return tagList.get(index);
+    public ArrayList<CharSequence> toList() {
+        return new ArrayList<>(tagMap.values());
     }
 }
