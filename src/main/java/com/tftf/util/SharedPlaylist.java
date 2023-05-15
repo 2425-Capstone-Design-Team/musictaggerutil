ea3ad4ea76fede2f80f5ccb230c71b4c35090341
jm0s <woals1488@naver.com>
2023-05-15 13:45:06 +0900

    PlaylistForShare -> SharedPlaylist 이름 변경

--- src/main/java/com/tftf/util/SharedPlaylist.java
+++ src/main/java/com/tftf/util/SharedPlaylist.java
@@ -2,11 +2,11 @@ package com.tftf.util;
 
 import java.util.ArrayList;
 
-public class PlaylistForShare extends Playlist {
+public class SharedPlaylist extends Playlist {
     public int likeCount;
     public int downloadCount;
 
-    public PlaylistForShare(String userID, String name, String description, ArrayList<Integer> musicList, int likeCount, int downloadCount) {
+    public SharedPlaylist(String userID, String name, String description, ArrayList<Integer> musicList, int likeCount, int downloadCount) {
         super(userID, name, description, musicList);
         this.likeCount = likeCount;
         this.downloadCount = downloadCount;
